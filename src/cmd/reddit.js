4827715b13e0b65482f2a2f174fed3c110a20219
aetheryx <zain1@live.nl>
2017-05-31 15:53:06 +0200

    Reddit: Reset arg, catching 404s properly, filtering nicer

--- src/cmd/reddit.js
+++ src/cmd/reddit.js
@@ -4,13 +4,26 @@ exports.run = async function(msg, args) {
     if (!args[0])
         return msg.edit('Missing required args.');
 
+    if (args[0] === 'reset')
+        return client.redditDB = new Array();
+
     if (args[1] && !['hour', 'day', 'today', 'week', 'month', 'year', 'all'].includes(args[1]))
         return msg.edit(`Argument error! \`${args[1]}\` is not one of \`hour | day | week | month | year | all\`.`);
 
-    const res = await snekfetch.get(`https://www.reddit.com/r/${args[0]}/top/.json?sort=top&t=${args[1] ? args[1] : 'all'}&limit=5`)
+    const res = await snekfetch.get(`https://www.reddit.com/r/${args[0]}/top/.json?sort=top&t=${args[1] ? args[1] : 'all'}&limit=8`)
+        .catch(err => {
+            if (err)
+                return msg.edit('Something went wrong. \n`' + err.message + '`')
+        });
 
-    const post = res.body.data.children.filter((child) => child.data.selftext.length <= 2000 && !client.redditDB.includes(child.data.id))[0];
-    
+    if (!res.request.path.startsWith('/r/'))
+        return msg.edit(`Subreddit \`${args[0]}\` not found.`)
+
+    const post = res.body.data.children
+        .filter((child) => child.data.selftext.length <= 2000)
+        .filter((child) => child.data.title.length <= 256)
+        .filter((child) => !client.redditDB.includes(child.data.id))[0];
+
     if (!post)
         return msg.edit('No new posts found!');
 
@@ -18,23 +31,29 @@ exports.run = async function(msg, args) {
 
     let imageURL;
 
-    if (post.preview)
-        imageURL = post.preview.images[0].source.url;
+    if (post.data.preview)
+        imageURL = post.data.preview.images[0].source.url;
 
-    msg.edit({ embed: {
-        color: settings.embedColor,
-        title: post.data.title,
-        url: post.data.url,
-        description: post.data.selftext,
-        image: { url: imageURL },
-        footer: { text: `${post.data.score} upvotes | ${post.data.num_comments} comments` }
-    } })
+    msg.edit({
+        embed: {
+            color: settings.embedColor,
+            title: post.data.title,
+            url: post.data.url,
+            description: post.data.selftext,
+            image: {
+                url: imageURL
+            },
+            footer: {
+                text: `${post.data.score} upvotes | ${post.data.num_comments} comments`
+            }
+        }
+    })
 
 };
 
 exports.props = {
-    name        : 'reddit',
-    usage       : '{prefix}reddit <subreddit name> [hour | day | week | month | year | all]',
-    aliases     : ['rd'],
-    description : 'Fetches text posts from a specific subreddit. Defaults to all-time unless specified.'
+    name: 'reddit',
+    usage: '{prefix}reddit <subreddit name> [hour | day | week | month | year | all]',
+    aliases: ['rd'],
+    description: 'Fetches posts from a specific subreddit. Defaults to all-time unless specified.'
 };
