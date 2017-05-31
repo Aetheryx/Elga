c5aaebcb77fc4e35b5c6bca3f1212b2349f61f49
aetheryx <zain1@live.nl>
2017-05-31 15:53:38 +0200

    Reimported version for Discord.version, const over let

--- src/cmd/stats.js
+++ src/cmd/stats.js
@@ -1,5 +1,6 @@
 const os = require('os');
 const moment = require('moment');
+const { version } = require('discord.js');
 require('moment-duration-format');
 
 exports.run = function(msg) {
@@ -13,25 +14,24 @@ exports.run = function(msg) {
     } else 
       uptime = moment.duration(process.uptime(), 'seconds').format('dd:hh:mm:ss');
 
-    let embed = {
+    const embed = {
         color: settings.embedColor,
         title: `Elga v${settings.version}`,
         fields: [
             { name: 'Uptime', value: uptime, inline: true },
             { name: 'Websocket Ping', value: client.ping.toFixed() + 'ms', inline: true },
             { name: 'RAM Usage', value: `${((process.memoryUsage().rss / 1024) / 1024).toFixed()}MB/${os.totalmem() > 1073741824 ? (os.totalmem() / 1024 / 1024 / 1024).toFixed() + 'GB' : (os.totalmem() / 1024 / 1024).toFixed() + 'MB'}(${(process.memoryUsage().rss / os.totalmem() * 100).toFixed(2)}%)`, inline: true },
-            { name: 'Libraries', value: `[Discord.js](https://discord.js.org) v${Discord.version}\nNode.js ${process.version}`, inline: true },
+            { name: 'Libraries', value: `[Discord.js](https://discord.js.org) v${version}\nNode.js ${process.version}`, inline: true },
             { name: 'System Info', value: `${process.platform} (${process.arch})`, inline: true },
             { name: '\u200b', value: '\u200b', inline: true }
         ]
     };
-
-    msg.edit({ embed: embed });
+    msg.edit({ embed });
 };
 
 exports.props = {
-aliases: ['info'],
-name: 'stats',
-description: 'Returns statistics about the selfbot.',
-usage: '{prefix}stats'
+    name        : 'stats',
+    usage       : '{prefix}stats',
+    aliases     : ['info'],
+    description : 'Returns statistics about the selfbot.'
 };
