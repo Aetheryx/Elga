ed182b310c30947f3add8c61a68c36387a50b839
Aetheryx <zain1@live.nl>
2017-06-02 09:06:16 +0200

    Emoji command added

---
+++ src/cmd/emoji.js
@@ -0,0 +1,38 @@
+exports.run = function (msg, args) {
+    if (!args[0])
+        return msg.edit('No arguments passed.');
+
+    const specialCodes = {
+        '0': ':zero: ',
+        '1': ':one: ',
+        '2': ':two: ',
+        '3': ':three: ',
+        '4': ':four: ',
+        '5': ':five: ',
+        '6': ':six: ',
+        '7': ':seven: ',
+        '8': ':eight: ',
+        '9': ':nine: ',
+        '#': ':hash: ',
+        '*': ':asterisk: ',
+        '?': ':grey_question: ',
+        '!': ':grey_exclamation: '
+    };
+    
+    const newContent = args.join(' ').toLowerCase().split('').map(letter => {
+        if (/[a-z]/g.test(letter)) 
+            return `:regional_indicator_${letter}: `;
+        else if (specialCodes[letter])
+            return specialCodes[letter];
+        return letter;
+    }).join('');
+
+    msg.edit(newContent);
+};
+
+exports.props = {
+    name        : 'emoji',
+    usage       : '{command} <text to turn into emojis>',
+    aliases     : ['emojify'],
+    description : 'Turns text into emojis.'
+};
