f14250db8b7bfdc098dcf8eee8c1a0585a818509
Aetheryx <zain1@live.nl>
2017-07-26 23:18:31 +0200

    Logger is a fn instead of a class, shorter usage

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,14 +1,11 @@
 /* eslint-disable no-console */
 const chalk = require('chalk');
-module.exports = class ElgaLogger {
-    constructor () {}
-    info (text) {
-        console.log(chalk.green(`[${Date().toString().split(' ').slice(1, 5).join(' ')}, INFO] `) + text);
-    }
-    warn (text) {
-        console.warn(chalk.yellow(`[${Date().toString().split(' ').slice(1, 5).join(' ')}, WARN] `) + text);
-    }
-    error (text) {
-        console.error(chalk.red(`[${Date().toString().split(' ').slice(1, 5).join(' ')}, ERROR] `) + text);
+module.exports = function (str, type) {
+    if (type === 'warn') {
+        console.warn(chalk.yellow(`[${Date().toString().split(' ').slice(1, 5).join(' ')}, WARN] `) + str);
+    } else if (type === 'error') {
+        console.error(chalk.red(`[${Date().toString().split(' ').slice(1, 5).join(' ')}, ERROR] `) + str);
+    } else {
+        console.log(chalk.green(`[${Date().toString().split(' ').slice(1, 5).join(' ')}, INFO] `) + str);
     }
 };
