9d4285f9351d9ee65eebbc7225e8693b8a367058
Aetheryx <zain1@live.nl>
2017-07-26 23:17:49 +0200

    Cleaning repo

---
+++ npm/elga.js
@@ -0,0 +1,3 @@
+module.exports = function () {
+    return 'As of right now, Elga is not available to the general public yet; this package is simply a placeholder. Elga is still being written as we speak.\nHowever, if you\'re for some reason *really* interested in getting yourself a test build of Elga, you can contact me at Aetheryx#2222.';
+};
